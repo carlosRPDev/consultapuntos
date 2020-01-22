ae4e7a54c2511033af19dc852640d14bb7455eb7
UcroX <ucroxlive@gmail.com>
2020-01-22 09:24:40 -0500

    Implementa la funcionalidad de autenticarse

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
 end
