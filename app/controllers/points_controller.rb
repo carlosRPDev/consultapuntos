b4fd05ee64878bc8f395749a06fad07220120d36
UcroX <ucroxlive@gmail.com>
2020-01-24 10:47:09 -0500

    Crea la ruta /points/new

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -2,4 +2,8 @@ class PointsController < ApplicationController
   def index
     @points = Point.all
   end
+
+  def new
+    @points = Point.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  root 'points#index'
+  get '/points', to: 'points#index'
 
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  get '/points/new', to: 'points#new'
 
-  resources :users, only: [:new, :create]
+  # get 'login', to: 'sessions#new'
+  # post 'login', to: 'sessions#create'
+  # delete 'logout', to: 'sessions#destroy'
+
+  # resources :users, only: [:new, :create]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
