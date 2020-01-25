699521507eb59e4bf33d86b06c253a74fd3d88be
UcroX <ucroxlive@gmail.com>
2020-01-25 11:35:23 -0500

    Actualiza la funcionalidad de autenticarse

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
-  resources :points
+  root 'points#index'
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
+  resources :users, only: [:new, :create]
+  resources :points
   # resources :users, only: [:new, :create]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'sessions#new'
 end
