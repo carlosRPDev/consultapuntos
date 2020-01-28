0c17fd5a7cbfda0cc2b580aab22879844eac2b2d
UcroX <ucroxlive@gmail.com>
2020-01-28 10:00:21 -0500

    Anota los modelos con los campos de la BD

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: points
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  ccosto      :string
+#  tecnologia  :string
+#  observacion :text
+#  ipradio     :string
+#  iptele      :string
+#  ipcom       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Point < ActiveRecord::Base
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  cuenta          :string
+#  password_digest :string
+#  name            :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
