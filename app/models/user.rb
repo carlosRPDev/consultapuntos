c247c9e5f11ec41c1c3c58830a643e0f931b3cb8
UcroX <ucroxlive@gmail.com>
2020-01-28 10:30:46 -0500

    Agrega validaciones a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,9 @@
 
 class User < ApplicationRecord
   has_secure_password validations: false
+
+  validates :cuenta, uniqueness: true
+  validates :password, presence: true, on: :create
+  validates :password, length: { in: 6..10 }, allow_nil: true
+  validates :name, presence: true
 end
